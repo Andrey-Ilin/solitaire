5e6bd06ad284cf941861d8f0887dc01d5d0e3b49
Andrey-Ilin <andrey.ilyin83@gmail.com>
2016-04-01 11:01:00 +0300

    first commit, now has some bags in logic

---
+++ frontend/js/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by andrej on 3/27/16.
+ */
+
+'use strict';
+
+let game = new Game({
+    "element": document.querySelector('[data-component="game"]')
+});
+
+game.toShuffleCards();
